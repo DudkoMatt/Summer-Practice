d6c3edf553f58618f4b393d51534ca87d93b3347
Matthew Dudko <dudko_mv@mail.ru>
2018-06-06 10:50:34 +0300

    PauseActivity: Макет + Переход

--- app/src/main/java/com/project/dudko/thebeastofthelabyrinth/PauseActivity.kt
+++ app/src/main/java/com/project/dudko/thebeastofthelabyrinth/PauseActivity.kt
@@ -1,12 +1,18 @@
 package com.project.dudko.thebeastofthelabyrinth
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pause.*
 
 class PauseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pause)
+        button_continue.setOnClickListener{
+            val intent = Intent(this, MainGameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
