4fcf4395746afcd6cbc33caa828b6dd170f1e51c
ftghmy <cfma454@gmail.com>
2018-06-06 12:36:19 +0300

    добавлен селектор
    Signed-off-by: ftghmy <cfma454@gmail.com>

---
+++ app/src/main/java/com/project/dudko/thebeastofthelabyrinth/LevelSelector.kt
@@ -0,0 +1,12 @@
+package com.project.dudko.thebeastofthelabyrinth
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LevelSelector : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_level_selector)
+    }
+}
