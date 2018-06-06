bbc7a00b5589e4a7999b0bece2dd61495165b254
ftghmy <cfma454@gmail.com>
2018-06-06 10:59:17 +0300

    Merge branch 'master' of https://github.com/DudkoMatt/Summer-Practice into Start_screen
    Signed-off-by: ftghmy <cfma454@gmail.com>
    
    # Conflicts:
    #	app/src/main/java/com/project/dudko/thebeastofthelabyrinth/FullscreenActivity.kt
    #	app/src/main/res/layout/activity_fullscreen.xml

--- app/src/main/java/com/project/dudko/thebeastofthelabyrinth/StartActivity.kt
+++ app/src/main/java/com/project/dudko/thebeastofthelabyrinth/StartActivity.kt
@@ -8,5 +8,6 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
     }
 }
