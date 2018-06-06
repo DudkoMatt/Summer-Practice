a2be049ac84f969a217b62d62f15bd28802e13c3
ftghmy <cfma454@gmail.com>
2018-06-06 13:20:27 +0300

    добавлен селектор
    Signed-off-by: ftghmy <cfma454@gmail.com>

--- app/src/main/java/com/project/dudko/thebeastofthelabyrinth/Help.kt
+++ app/src/main/java/com/project/dudko/thebeastofthelabyrinth/Help.kt
@@ -1,13 +1,19 @@
 package com.project.dudko.thebeastofthelabyrinth
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_help.*
 
 class Help : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+        button2.setOnClickListener {
+            finish()
 
+
+        }
     }
 }
