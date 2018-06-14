1b87f387c806e5fe3f44b7d00abdc93a613a27cd
Alexander <Al.lvov777@gmail.com>
2018-06-14 13:08:15 +0300

    More design changes

--- app/src/main/java/com/project/dudko/thebeastofthelabyrinth/EndOfTheLevelActivity.kt
+++ app/src/main/java/com/project/dudko/thebeastofthelabyrinth/EndOfTheLevelActivity.kt
@@ -12,12 +12,25 @@ class EndOfTheLevelActivity : AppCompatActivity() {
         setContentView(R.layout.activity_end_of_the_level)
 
 
+<<<<<<< Updated upstream
 
         button_start.setOnClickListener{
             val intent = Intent(this, StartActivity::class.java)
             startActivity(intent)
         }
 
+=======
+        Won.text = Won.text.toString().replace("level!", "level ${intent.getStringExtra("Level")}!")
+        //results.text = "Your results:\nCoins: ${intent.getStringExtra("Coins")}; Time: ${intent.getStringExtra("Time")}; Turns: ${intent.getStringExtra("Turns")}"
+        results.text = "Your results:\nCoins: ${intent.getStringExtra("Coins")}" +
+                " Turns: ${intent.getStringExtra("Turns")}"
+        results.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        /*button_start.setOnClickListener{
+            setResult(1)
+            finish()
+        }*/
+        setResult(1)
+>>>>>>> Stashed changes
         button_levels.setOnClickListener{
             val intent = Intent(this, LevelSelector::class.java)
             startActivity(intent)
